5b6c77f2d05f262d40e8391b8fd815d403b557af
Débora Ferreira <assisdebora123@gmail.com>
2024-06-21 18:30:05 -0300

    ajustes

--- glaucoma_yolov8.py
+++ glaucoma_yolov8.py
@@ -8,6 +8,7 @@ from ultralytics import YOLO
 from metricas_disco_escavacao import CDR, RDR, NRR, BVR, BVR2 , CDRvh, excentricidade
 from xgboost import XGBClassifier
 
+
 def contours_(x_min, y_min, x_max, y_max):
     # Calcular o centro da caixa delimitadora
     center_x = (x_min + x_max) / 2
@@ -19,33 +20,33 @@ def contours_(x_min, y_min, x_max, y_max):
 
     return center_x, center_y, semi_axis_x, semi_axis_y
 
-def draw_contours(image, result_disc, result_cup):
+def draw_contours(image, results_disc, results_cup):
     new_img = image.copy()
     altura, largura, _ = image.shape
     mask_disc = np.zeros((altura, largura), dtype=np.uint8)
+
+    for result in results_disc:
+        for bbox in result.boxes.xyxy:
+            x_min, y_min, x_max, y_max = map(int, bbox.tolist())
+            # Desenhar a elipse na imagem
+            center_x, center_y, semi_axis_x, semi_axis_y = contours_(x_min, y_min, x_max, y_max)
+            cv2.ellipse(image, (int(center_x), int(center_y)), (int(semi_axis_x), int(semi_axis_y)), 0, 0, 360, (0,  255,0) , 2)
+            cv2.ellipse(mask_disc, (int(center_x), int(center_y)), (int(semi_axis_x), int(semi_axis_y)), 0, 0, 360, (255,255,255), -1)
+            cnts_disc = cv2.findContours(mask_disc, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts_disc = imutils.grab_contours(cnts_disc)
+            cnt_disc = max(cnts_disc, key=cv2.contourArea)
     mask_cup = np.zeros((altura, largura), dtype=np.uint8)
+    for result2 in results_cup:
+        for bbox2 in result2.boxes.xyxy:
+            x_min2, y_min2, x_max2, y_max2 = map(int, bbox2.tolist())
 
-    # Considerar apenas a região de maior confiança
-    if len(result_disc.boxes) > 0:
-        best_disc = max(result_disc.boxes, key=lambda x: x.conf[0])
-        x_min, y_min, x_max, y_max = map(int, best_disc.xyxy[0].tolist())
-        center_x, center_y, semi_axis_x, semi_axis_y = contours_(x_min, y_min, x_max, y_max)
-        cv2.ellipse(image, (int(center_x), int(center_y)), (int(semi_axis_x), int(semi_axis_y)), 0, 0, 360, (0, 255, 0), 2)
-        cv2.ellipse(mask_disc, (int(center_x), int(center_y)), (int(semi_axis_x), int(semi_axis_y)), 0, 0, 360, (255, 255, 255), -1)
-        cnts_disc = cv2.findContours(mask_disc, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts_disc = imutils.grab_contours(cnts_disc)
-        cnt_disc = max(cnts_disc, key=cv2.contourArea)
+            center_x2, center_y2, semi_axis_x2, semi_axis_y2 = contours_(x_min2, y_min2, x_max2, y_max2)
+            cv2.ellipse(image, (int(center_x2), int(center_y2)), (int(semi_axis_x2), int(semi_axis_y2)), 0, 0, 360, (0, 255,0) , 2)
+            cv2.ellipse(mask_cup, (int(center_x2), int(center_y2)), (int(semi_axis_x2), int(semi_axis_y2)), 0, 0, 360, (255,255,255), -1)
 
-    if len(result_cup.boxes) > 0:
-        best_cup = max(result_cup.boxes, key=lambda x: x.conf[0])
-        x_min2, y_min2, x_max2, y_max2 = map(int, best_cup.xyxy[0].tolist())
-        center_x2, center_y2, semi_axis_x2, semi_axis_y2 = contours_(x_min2, y_min2, x_max2, y_max2)
-        cv2.ellipse(image, (int(center_x2), int(center_y2)), (int(semi_axis_x2), int(semi_axis_y2)), 0, 0, 360, (0, 255, 0), 2)
-        cv2.ellipse(mask_cup, (int(center_x2), int(center_y2)), (int(semi_axis_x2), int(semi_axis_y2)), 0, 0, 360, (255, 255, 255), -1)
-        cnts_cup = cv2.findContours(mask_cup, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts_cup = imutils.grab_contours(cnts_cup)
-        cnt_cup = min(cnts_cup, key=cv2.contourArea)
-
+            cnts_cup = cv2.findContours(mask_cup, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts_cup = imutils.grab_contours(cnts_cup)
+            cnt_cup = min(cnts_cup, key=cv2.contourArea)
 
     cdr = CDR(cnt_disc, cnt_cup)
     cdrv, cdrh = CDRvh(cnt_disc, cnt_cup)
@@ -54,7 +55,7 @@ def draw_contours(image, result_disc, result_cup):
     return cdr, cdrv, cdrh, rdr, nrr
 
 
-    
+
 # Função principal para processar a imagem
 def process_image(image_bytes, yolo_model_disc, yolo_model_cup, confidence_threshold=0.1):
     # Converter bytes para imagem
@@ -111,5 +112,3 @@ if image_bytes is not None:
         col3.write(f"NRR: {nrr}")
     if res is not None:
         col3.write(f"Result: {res}")
-
-
