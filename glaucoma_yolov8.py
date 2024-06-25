1d3e0e4d022ff45ba93575132c5518f12ff9cb22
Débora Ferreira <assisdebora123@gmail.com>
2024-06-25 10:32:40 -0300

    atualizado

--- glaucoma_yolov8.py
+++ glaucoma_yolov8.py
@@ -4,18 +4,45 @@ import joblib
 import imutils
 import numpy as np
 from PIL import Image
+from io import BytesIO
 from ultralytics import YOLO
 from metricas_disco_escavacao import CDR, RDR, NRR, BVR, BVR2 , CDRvh, excentricidade
 from xgboost import XGBClassifier
 
-def crop_image_with_margin(image, box, margin=10):
+
+
+
+
+def crop_image_with_margin(image, box):
     width, height = image.size
     x1, y1, x2, y2 = box
-    x1 = max(0, x1 - margin)
-    y1 = max(0, y1 - margin)
-    x2 = min(width, x2 + margin)
-    y2 = min(height, y2 + margin)
-    return image.crop((x1, y1, x2, y2))
+
+    # Calcular o centro da caixa delimitadora
+    center_x = (x1 + x2) / 2
+    center_y = (y1 + y2) / 2
+
+    # Calcular o raio como metade da distância entre os pontos extremos da caixa delimitadora
+    radius_x = (x2 - x1) / 2
+    radius_y = (y2 - y1) / 2
+
+    # Definir a margem como metade do raio
+    margin_x = int(radius_x )
+    margin_y = int(radius_y )
+
+    # Expandir a caixa delimitadora com a margem desejada
+    x1_margin = max(0, int(center_x - radius_x - margin_x))
+    y1_margin = max(0, int(center_y - radius_y - margin_y))
+    x2_margin = min(width, int(center_x + radius_x + margin_x))
+    y2_margin = min(height, int(center_y + radius_y + margin_y))
+
+    # Garantir que o recorte esteja dentro dos limites da imagem
+    x1_crop = max(0, x1_margin)
+    y1_crop = max(0, y1_margin)
+    x2_crop = min(width, x2_margin)
+    y2_crop = min(height, y2_margin)
+
+    return image.crop((x1_crop, y1_crop, x2_crop, y2_crop))
+
 
 
 
@@ -63,19 +90,29 @@ def draw_contours(image, result_disc, result_cup):
     nrr = NRR(cnt_disc, cnt_cup, new_img)
     return cdr, cdrv, cdrh, rdr, nrr
 
+def pil_image_to_bytes(image):
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG")
+    buffer.seek(0)
+    return buffer
 
 
 def img_retina(image):
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image_ = Image.open(image)
+    image_rgb = np.array(image_)
+
     # pixels pretos
-    black_pixels = np.sum(np.all(image_rgb == [0, 0, 0], axis=2))
+    black_pixels = np.sum(np.all(image_rgb  <= [20, 20, 20], axis=2))
     # número total de pixels
     total_pixels = image_rgb.shape[0] * image_rgb.shape[1]
     # proporção de pixels pretos
     black_pixel_ratio = black_pixels / total_pixels
+    print(black_pixel_ratio)
 
-    if black_pixel_ratio > 0.1:
-        results_cup = yolo_model_cup(image, conf=0.01)
+    if black_pixel_ratio > 0.05:
+
+        results_cup = yolo_model_cup(image_, conf=0.01)
         # Verificar se há detecções
         if results_cup and len(results_cup[0].boxes.xyxy) > 0:
             # Encontrar a detecção com a maior confiança
@@ -91,17 +128,21 @@ def img_retina(image):
                 box = best_box.cpu().numpy().astype(int).tolist()
                 
                 # Recortar a imagem com a margem
-                image_final = crop_image_with_margin(image, box, 10)
+                cropped_image = crop_image_with_margin(image_, box)
+                
+    #             # Converter a imagem recortada para bytes
+                image_final = pil_image_to_bytes(cropped_image)
     else:
         image_final = image
 
     return image_final
 # Função principal para processar a imagem
 def process_image(image_bytes, yolo_model_disc, yolo_model_cup, confidence_threshold=0.01):
+    
+    path_input = img_retina(image_bytes)
+
     # Converter bytes para imagem
-    image_in = Image.open(image_bytes)
-    # image = image.resize((640,640))
-    image = img_retina(image_in)
+    image = Image.open(path_input)
     image_np = np.array(image)
 
     # Processar imagem com os modelos YOLO para discos e copos
@@ -112,9 +153,11 @@ def process_image(image_bytes, yolo_model_disc, yolo_model_cup, confidence_thres
     cdr, cdrv, cdrh, rdr, nrr = draw_contours(image_np, results_disc[0], results_cup[0])
 
     pred = loaded_model.predict([[cdr, cdrv, cdrh, rdr,nrr]])
-    if pred == 0:
+    if pred == 0 and cdr < 0.35:
         res = 'Normal'
     elif pred==1:
+        res = 'Glaucoma'
+    else:
         res = 'Glaucoma'
 
     return image_np, cdr, cdrv, cdrh, rdr,nrr, res
